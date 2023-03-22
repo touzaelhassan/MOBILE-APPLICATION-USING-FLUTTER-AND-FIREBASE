a5a326e51faa39b8b6af7b1c5f86b043f74ab1ef
touzaelhassan <touzaelhassan@gmail.com>
2023-03-22 20:45:33 +0000

    update

---
+++ lib/welcome_sreen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Image.asset("images/boy.png"),
+          SizedBox(
+              height: 5
+          ),
+          Text(
+              "Order Your Food Now !",
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Text(
+                "Order food and get delivery within a few minutes to your door.",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black45,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(height: 80),
+          InkWell(
+            onTap: (){
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomeScreen(),
+                  ));
+            },
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+              padding: EdgeInsets.symmetric(vertical: 20),
+              decoration: BoxDecoration(
+                color: Colors.black87,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                      "GET STARTED",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.white,
+                    size: 20,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
